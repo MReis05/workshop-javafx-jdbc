9797da826688dedbce499e7a5e3001f984959fa3
Matheus Reis <matheusreiscrs@gmail.com>
2025-05-07 14:34:44 -0300

    Saving a new Department

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -27,32 +27,34 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 	public void insert(Department obj) {
 		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement("INSERT INTO department "
-		            + "(Id, Name) "
-					+ "VALUES " + "(?, ?)", Statement.RETURN_GENERATED_KEYS);
+			st = conn.prepareStatement(
+				"INSERT INTO department " +
+				"(Name) " +
+				"VALUES " +
+				"(?)", 
+				Statement.RETURN_GENERATED_KEYS);
 
-			st.setInt(1, obj.getId());
-			st.setString(2, obj.getName());
+			st.setString(1, obj.getName());
 
 			int rowsAffected = st.executeUpdate();
-
+			
 			if (rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				if (rs.next()) {
 					int id = rs.getInt(1);
 					obj.setId(id);
 				}
-				Db.closeResultSet(rs);
-			} else {
-				throw new DbException("Unexpected error!! No rows affecteed");
 			}
-
-		} catch (SQLException e) {
+			else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		}
+		catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		} finally {
+		} 
+		finally {
 			Db.closeStatement(st);
 		}
-		
 	}
 
 	@Override
